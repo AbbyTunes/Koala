06453001100620e854fc1cc799656631fb233246
ray-leun1 <ray.leun1@gmail.com>
2019-10-23 14:58:30 -0700

    Work on answer_actions

---
+++ frontend/src/actions/answer_actions.js
@@ -0,0 +1,35 @@
+import * as AnswerApiUtil from '../util/answer_api_util';
+
+export const RECEIVE_ANSWERS = 'RECEIVE_ANSWERS';
+export const RECEIVE_ANSWER = 'RECEIVE_ANSWER';
+export const REMOVE_ANSWER = 'REMOVE_ANSWER';
+
+const receiveAnswers = (answers) => ({
+  type: RECEIVE_ANSWERS,
+  answers: answers.data
+});
+
+const receiveAnswer = (answer) => ({
+  type: RECEIVE_ANSWER,
+  answer: answer.data
+});
+
+export const fetchAnswers = () => dispatch => (
+  AnswerApiUtil.fetchAnswers().then(answers => dispatch(receiveAnswers(answers)))
+);
+
+export const fetchAnswer = (id) => dispatch => (
+  AnswerApiUtil.fetchAnswer(id).then(answer => dispatch(receiveAnswer(answer)))
+);
+
+export const createAnswer = (answer) => dispatch => (
+  AnswerApiUtil.createAnswer(answer).then(answer => dispatch(receiveAnswer(answer)))
+);
+
+export const updateAnswer = (answer) => dispatch => (
+  AnswerApiUtil.updateAnswer(answer).then(answer => dispatch(receiveAnswer(answer)))
+);
+
+export const deleteAnswer = (answerId) => dispatch => (
+  AnswerApiUtil.deleteAnswer(answerId).then(answer => dispatch({ type: REMOVE_ANSWER, answerId: answer.id }))
+);
