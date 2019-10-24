280b074c8a7612404fb18cf87f46c78fc529722e
Ryan Woods <ryanwoods14@gmail.com>
2019-10-24 12:38:40 -0400

    current user add

--- frontend/src/components/profile/profile_container.js
+++ frontend/src/components/profile/profile_container.js
@@ -1,14 +1,14 @@
 import { connect } from 'react-redux';
 import { logout } from '../../actions/session_actions';
 
-import NavBar from './navbar';
+import Profile from './profile';
 
 const mapStateToProps = state => ({
-	loggedIn: state.session.isAuthenticated,
-	currentUser: state.session.user
+  loggedIn: state.session.isAuthenticated,
+  currentUser: state.session.user
 });
 
 export default connect(
-	mapStateToProps,
-	{ logout }
-)(NavBar);
+  mapStateToProps,
+  { logout }
+)(Profile);
