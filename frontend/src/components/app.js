502dabdd95f9bdc7227056c8fc02186f70e403ce
Abby Xu <abby.tsui2016@gmail.com>
2019-10-24 09:31:39 -0700

    reset styling

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,7 +3,8 @@ import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Route, Switch, Redirect, withRouter } from "react-router-dom";
 
 import MainPage from './main/main_page';
-import SessionFormContainer from './session/session_form_container'
+import SessionFormContainer from './session/session_form_container';
+import ResetStylesheet from '../stylesheets/reset.css';
 
 // added props for stlying based on pathname
 const App = (props) => {
