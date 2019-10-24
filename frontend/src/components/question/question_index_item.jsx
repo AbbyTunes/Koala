9dffd3ee6b69b1fee4783d21ce2ca3979c449313
Abby Xu <abby.tsui2016@gmail.com>
2019-10-24 10:07:06 -0700

    ready to switch branch

--- frontend/src/components/question/question_index_item.jsx
+++ frontend/src/components/question/question_index_item.jsx
@@ -20,6 +20,14 @@ const QuestionIndexItem = ({ question, deleteQuestion }) => {
 						<Link to="/">topic_name_hard_code</Link>
 						{/* <div>{question.topics}</div> */}
 					</div>
+					<div className="question-hide">
+						<svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
+							<g id="small_close" class="icon_svg-stroke" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke="#666666" stroke-width="1.5">
+								<path d="M12,6 L12,18" transform="translate(12.000000, 12.000000) rotate(45.000000) translate(-12.000000, -12.000000) "></path>
+								<path d="M18,12 L6,12" transform="translate(12.000000, 12.000000) rotate(45.000000) translate(-12.000000, -12.000000) "></path>
+							</g>
+						</svg>
+					</div>
 				</div>
 				<div className="question-2"><Link to="/">{question.title}</Link></div>
 				<div className="question-3">
@@ -28,18 +36,17 @@ const QuestionIndexItem = ({ question, deleteQuestion }) => {
 						. Last created { timeAgo.format(Date.now() - 60 * 1000, 'time') } ago 
 					</div>
 				</div>
-				<ul className="question-4">
-					<li>Follow</li> 
-					<li>Share</li>
-					<li>Comment</li>
-				</ul>
-			</div>
-			<div className="question-right">
-				<div className="question-hide"></div>
-				<ul className="question-more">
-					<li>downvote</li>
-					<li>more</li>
-				</ul>
+				<div className="question-4"> 
+					<ul>
+						<li>Follow</li> 
+						<li>Share</li>
+						<li>Comment</li>
+					</ul>
+					<ul className="question-more">
+						<li>downvote</li>
+						<li>more</li>
+					</ul>
+				</div>
 			</div>
 		</li>
 	)
