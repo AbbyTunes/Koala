dbfc29b5ee251bd3666a05ff726479104690ee29
Ryan Woods <ryanwoods14@gmail.com>
2019-10-29 10:58:42 -0400

    navbar prod styling

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import headerLogo from '../../images/Koala-logo-final.png'
 import profileIcon from '../../images/koala-profile.png'
+import githubIcon from '../../images/github-logo.png'
 import '../../stylesheets/reset.css' // this will probably get moved
 import '../../stylesheets/navbar.css'
 import QuestionPopUp from '../question/forms/question_pop_up'
@@ -126,18 +127,14 @@ class NavBar extends React.Component {
 
 								<span id="nav-item-span">
 									<div className="header-nav-item">
-										<Link className="nav-item-link" to={'/filler2'}>
+										<a className="nav-item-link github" href={"https://github.com/AbbyTunes/Koala"}>
 											<div className="nav-item-link-inner">
 												<span className="nav-item-link-icon">
-													<svg version="1.1" baseProfile="full" width="24" height="24" xmlns="http://www.w3.org/2000/svg">
-														<g className="nav-item-link-icon-svg-g">
-															<path d="M3.56993697,19.0927202 L4.90727984,20.430063 L6.32212519,19.8415186 L21.6540999,4.51430825 C22.0414488,4.12695944 22.1927257,3.56238623 22.0509462,3.0332579 C21.9091667,2.50412957 21.4958704,2.09083329 20.9667421,1.94905379 C20.4376281,1.80727812 19.8730711,1.95854305 19.4857232,2.34586864 L4.15848145,17.6778748 L3.56993697,19.0927202 Z M3.04166667,3.04166667 L3.04166667,13.4375 C3.04166667,13.782678 2.76184464,14.0625 2.41666667,14.0625 C2.0714887,14.0625 1.79166667,13.782678 1.79166667,13.4375 L1.79166667,2.41666667 C1.79166667,2.0714887 2.0714887,1.79166667 2.41666667,1.79166667 L12.4791667,1.79166667 C12.8243446,1.79166667 13.1041667,2.0714887 13.1041667,2.41666667 C13.1041667,2.76184464 12.8243446,3.04166667 12.4791667,3.04166667 L3.04166667,3.04166667 Z M20.9583333,20.9583333 L20.9583333,10.5625 C20.9583333,10.217322 21.2381554,9.9375 21.5833333,9.9375 C21.9285113,9.9375 22.2083333,10.217322 22.2083333,10.5625 L22.2083333,21.5833333 C22.2083333,21.9285113 21.9285113,22.2083333 21.5833333,22.2083333 L11.5208333,22.2083333 C11.1756554,22.2083333 10.8958333,21.9285113 10.8958333,21.5833333 C10.8958333,21.2381554 11.1756554,20.9583333 11.5208333,20.9583333 L20.9583333,20.9583333 Z M18.6017396,1.46208528 C19.3049382,0.758886609 20.3297725,0.484283075 21.2902659,0.741646501 C22.2507593,0.999009928 23.0009901,1.74924069 23.2583535,2.7097341 C23.5157169,3.67022752 23.2411134,4.69506179 22.5379147,5.39826041 L7.11833139,20.8130521 C7.06053277,20.8708327 6.99196292,20.9167166 6.91650427,20.9481057 L1.69837927,23.1187307 C1.18343214,23.3329374 0.667062601,22.8165679 0.881269257,22.3016207 L3.05189426,17.0834957 C3.08328339,17.0080371 3.12916726,16.9394672 3.18694792,16.8816686 L18.6017396,1.46208528 Z"></path>
-														</g>
-													</svg>
+													<img className="profile-photo-img" src={githubIcon} alt="Github logo" />
 												</span>
 											</div>
-											<span>Filler</span>
-										</Link>
+											<span>Github</span>
+										</a>
 									</div>
 								</span>
 							</div>
@@ -200,12 +197,12 @@ class NavBar extends React.Component {
 																</Link>
 															</div>
 															<div className="nav-profile-dropdown-links-container">
-																<Link className="nav-profile-dropdown-links-link" to={"/messages"}>
+																<a className="nav-profile-dropdown-links-link" href={"https://github.com/AbbyTunes/Koala"}>
 																	<div className="nav-profile-dropdown-links-link-content">
-																		Messages
+																		Github
 																	</div>
-																</Link>
-																<Link className="nav-profile-dropdown-links-link" to={"/create_ad"}>
+																</a>
+																{/* <Link className="nav-profile-dropdown-links-link" to={"/create_ad"}>
 																	<div className="nav-profile-dropdown-links-link-content">
 																		Create Ad
 																	</div>
@@ -224,16 +221,16 @@ class NavBar extends React.Component {
 																	<div className="nav-profile-dropdown-links-link-content">
 																		Bookmarks
 																	</div>
-																</Link>
-																<Link className="nav-profile-dropdown-links-link" to={"/drafts"}>
+																</Link> */}
+																<a className="nav-profile-dropdown-links-link" href={"https://www.savethekoala.com/"}>
 																	<div className="nav-profile-dropdown-links-link-content">
-																		Drafts
+																		Donate
 																	</div>
-																</Link>
+																</a>
 															</div>
 
 															<div className="nav-profile-dropdown-settings-links-container">
-																<Link className="nav-profile-dropdown-settings-link" to={"/settings"}>
+																{/* <Link className="nav-profile-dropdown-settings-link" to={"/settings"}>
 																	<div className="nav-profile-dropdown-settings-link-content">
 																		Settings
 																	</div>
@@ -247,7 +244,7 @@ class NavBar extends React.Component {
 																	<div className="nav-profile-dropdown-settings-link-content">
 																		Help
 																	</div>
-																</Link>
+																</Link> */}
 																<div className="nav-profile-dropdown-settings-link" onClick={this.logoutUser}>
 																	<div className="nav-profile-dropdown-settings-link-content" >
 																		Logout
@@ -257,29 +254,29 @@ class NavBar extends React.Component {
 
 															<div className="nav-profile-dropdown-official-links-container">
 																<div className="nav-profile-dropdown-official-links-wrapper">
-																	<Link className="nav-profile-dropdown-official-links-link" to={"/about"}>
-																		About
+																	<a className="nav-profile-dropdown-official-links-link" href={"https://www.savethekoala.com/sites/savethekoala.com/files/uploads/old/images/photogallery/photosale0024.jpg"}>
+																		Koala 1
 																		<span className="bullet"> · </span>
-																	</Link>
-																	<Link className="nav-profile-dropdown-official-links-link" to={"/careers"}>
-																		Careers
+																	</a>
+																	<a className="nav-profile-dropdown-official-links-link" href={"https://www.savethekoala.com/sites/savethekoala.com/files/uploads/old/images/photogallery/photosale0023.jpg"}>
+																		Koala 2
 																		<span className="bullet"> · </span>
-																	</Link>
-																	<Link className="nav-profile-dropdown-official-links-link" to={"/terms"}>
-																		Terms
+																	</a>
+																	<a className="nav-profile-dropdown-official-links-link" href={"https://www.savethekoala.com/sites/savethekoala.com/files/uploads/old/images/photogallery/photosale0025.jpg"}>
+																		Koala 3
 																		<span className="bullet"> · </span>
-																	</Link>
-																	<Link className="nav-profile-dropdown-official-links-link" to={"/privacy"}>
-																		Privacy
+																	</a>
+																	<a className="nav-profile-dropdown-official-links-link" href={"https://www.savethekoala.com/sites/savethekoala.com/files/styles/slide_image/public/imageblock/5.png?itok=x0ja2MKc"}>
+																		Koala 4
 																		<span className="bullet"> · </span>
-																	</Link>
-																	<Link className="nav-profile-dropdown-official-links-link" to={"/acceptable_use"}>
-																		Acceptable Use
+																	</a>
+																	{/* <Link className="nav-profile-dropdown-official-links-link" to={"/acceptable_use"}>
+																		Koala 5
 																		<span className="bullet"> · </span>
 																	</Link>
 																	<Link className="nav-profile-dropdown-official-links-link" to={"/businesses"}>
-																		Businesses
-																	</Link>
+																	 Koala 6
+																	</Link> */}
 																</div>
 															</div>
 														</div>
