34bb0d4769f3752bbdcf9aaf058f9f5bcfe2ae3a
Abby Xu <abby.tsui2016@gmail.com>
2019-12-09 21:10:59 -0800

    changing colors

--- frontend/src/components/nav/credit.jsx
+++ frontend/src/components/nav/credit.jsx
@@ -12,30 +12,45 @@ const Credit = () => {
 				<div className="contributor-1 ">
 					<div className="credit-name-1">Jun Xu (Abby)</div>
 					<div className="credit-links-1">
-						<div className="credit-icon-linkedin"></div>
-						<div className="credit-icon-github"></div>
-						<div className="credit-icon-facebook"></div>
-						<div className="credit-icon-angellist"></div>	
+						<a href="https://www.linkedin.com/in/jun-xu-6a204466/" className="credit-a-linkedin">
+							<div className="credit-icon-linkedin"></div>
+						</a>
+						<a href="https://github.com/AbbyTunes" className="credit-a-angellist">
+							<div className="credit-icon-angellist"></div>	
+						</a>
+						<a href="https://github.com/AbbyTunes" className="credit-a-github">
+							<div className="credit-icon-github"></div>
+						</a>
 					</div>
 				</div>
 
 				<div className="contributor-2 ">
-					<div className="credit-name-2">Raymond Leung</div>
+					<div className="credit-name-2">Name</div>
 					<div className="credit-links-2">
-						<div className="credit-icon-linkedin"></div>
-						<div className="credit-icon-github"></div>
-						<div className="credit-icon-facebook"></div>
-						<div className="credit-icon-angellist"></div>
+						<a href="" className="credit-a-linkedin">
+							<div className="credit-icon-linkedin"></div>
+						</a>
+						<a href="" className="credit-a-angellist">
+							<div className="credit-icon-angellist"></div>
+						</a>
+						<a href="" className="credit-a-github">
+							<div className="credit-icon-github"></div>
+						</a>
 					</div>
 				</div>
 
-				<div className="contributor-1 ">
-					<div className="credit-name-1">Ryan Woods</div>
-					<div className="credit-links-1">
-						<div className="credit-icon-linkedin"></div>
-						<div className="credit-icon-github"></div>
-						<div className="credit-icon-facebook"></div>
-						<div className="credit-icon-angellist"></div>
+				<div className="contributor-3 ">
+					<div className="credit-name-3">Name</div>
+					<div className="credit-links-3">
+						<a href="" className="credit-a-linkedin">
+							<div className="credit-icon-linkedin"></div>
+						</a>
+						<a href="" className="credit-a-angellist">
+							<div className="credit-icon-angellist"></div>
+						</a>
+						<a href="" className="credit-a-github">
+							<div className="credit-icon-github"></div>
+						</a>
 					</div>
 				</div>
 
