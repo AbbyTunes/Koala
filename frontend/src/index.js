22227cc72cea10bb5623829112f577d77f597ea7
Ryan Woods <ryanwoods14@gmail.com>
2020-01-08 13:07:36 -0500

    possible login url fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 		if (decodedUser.exp < currentTime) {
 			store.dispatch(logout());
-			window.location.href = '/login';
+			window.location.href = '/';
 		}
 	} else {
 		store = configureStore({});
